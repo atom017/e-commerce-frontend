65d5b6c401d40a3b74f6cd6f1cbff70b5c6f7399
atom017 <khaingjar@gmail.com>
2024-12-02 12:40:06 +0630

    [add] added Cancel Page

---
+++ src/components/CancelPage.jsx
@@ -0,0 +1,8 @@
+// src/pages/CancelPage.tsx
+import React from 'react';
+
+const CancelPage = () => {
+  return <div>Your payment was cancelled. Please try again.</div>;
+};
+
+export default CancelPage;
