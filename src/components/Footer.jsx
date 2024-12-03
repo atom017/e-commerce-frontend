08a5e664a8ea8f327081f8de77b2cc7263262a2d
atom017 <khaingjar@gmail.com>
2024-12-03 16:33:53 +0630

    [remove] remove unnecessary links on Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,7 +17,7 @@ const Footer = () => {
           </div>
 
           {/* Column 2: Links */}
-          <div>
+          {/* <div>
             <h3 className="text-lg font-bold mb-4">Quick Links</h3>
             <ul className="space-y-2">
               <li><a href="/" className="hover:text-gray-300 text-sm">Home</a></li>
@@ -26,7 +26,7 @@ const Footer = () => {
               <li><a href="/contact" className="hover:text-gray-300 text-sm">Contact Us</a></li>
               <li><a href="/terms" className="hover:text-gray-300 text-sm">Terms & Conditions</a></li>
             </ul>
-          </div>
+          </div> */}
 
           {/* Column 3: Contact */}
           <div>
